84c93ef287bae6e76551159e49939830883e96ba
aetsmtl <arnold.fotsing.nde@esigetel.net>
2017-01-25 12:07:28 +0100

    using boolean and little refactorisation

--- src/main/java/com/aetsmtl/java8Learning/App.java
+++ src/main/java/com/aetsmtl/java8Learning/App.java
@@ -12,22 +12,33 @@ public class App
         String clientName = "Odilya";
         String message;
         
-        message = customerName + " says Hallo to " + clientName;
-        
         double priceCup = 12.4;
         double priceScreen = 67.2;
         double total;
+        
+        boolean isTakeCB;
+        
+        message = customerName + " says Hallo to " + clientName;
         
         total = priceCup*3 + priceScreen*5;
  
         System.out.println(message);
         System.out.println(customerName + " : Your bill is " + total);
         
-        if (total > 50){
+        if (total > 5000){
             System.out.println(clientName + ": " + total + "€! " + "its enought for me ");
         }
         else{
             System.out.println(clientName +": ok Do you take CB?" );
+            isTakeCB = false;
+            if (isTakeCB){
+            	System.out.println(customerName +": Of course yes!" );
+            	System.out.println(clientName +": Oh yes!" );
+            }
+            {
+            	System.out.println(customerName +": No, Sorry" );
+            	System.out.println(clientName +": Damn!... oops Sorry could you please remove some items?");
+            }
         }
         
     }
