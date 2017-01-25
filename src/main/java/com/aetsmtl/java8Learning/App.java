5b02b0c604ed6db26a98eb0002d2964c6a8748c0
aetsmtl <arnold.fotsing.nde@esigetel.net>
2017-01-25 11:42:30 +0100

    using String

--- src/main/java/com/aetsmtl/java8Learning/App.java
+++ src/main/java/com/aetsmtl/java8Learning/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String customerName = "Mike";
+        String clientName = "Odilya";
+        String message;
+        
+        message = customerName + " says Hallo to " + clientName;
+        
+        System.out.println(message);
+       
     }
 }
