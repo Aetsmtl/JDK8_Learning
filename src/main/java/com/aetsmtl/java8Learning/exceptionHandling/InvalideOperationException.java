6ec15a23909b2f45279927e6e176573860ebe957
aetsmtl <arnold.fotsing.nde@esigetel.net>
2017-02-03 11:29:28 +0100

    example of creation Exception

---
+++ src/main/java/com/aetsmtl/java8Learning/exceptionHandling/InvalideOperationException.java
@@ -0,0 +1,22 @@
+package com.aetsmtl.java8Learning.exceptionHandling;
+
+public class InvalideOperationException extends Exception {
+	
+	public InvalideOperationException() {
+		// TODO Auto-generated constructor stub
+		super ();
+	}
+	public InvalideOperationException(String message) {
+		// TODO Auto-generated constructor stub
+		super (message);
+	}
+	public InvalideOperationException(Throwable cause){
+		// TODO Auto-generated constructor stub
+		super (cause);
+	}
+	public InvalideOperationException(String message, Throwable cause) {
+		// TODO Auto-generated constructor stub
+		super (message, cause);
+	}
+	
+}
